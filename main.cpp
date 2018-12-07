4736a88c9f33e7fd22a36a222ff8a4f073221517
shumaym <shumaym@protonmail.com>
2018-12-07 15:01:56 -0600

    Added long option variants and the ability to change the screen dimensions.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,25 @@
 #include "math.h"
 #include <unordered_map>
 #include <unistd.h>
+#include <getopt.h>
+#include <typeinfo>
+#include <sstream>
 
 typedef struct { uint16_t x; uint16_t y; } Vertex;
 
+const option long_opts[] = {
+	{"vertices", 1, 0, 'v'},
+	{"fraction", 1, 0, 'f'},
+	{"stepping", 1, 0, 's'},
+	{"frame-delay", 1, 0, 'd'},
+	{"dimensions", 1, 0, 'z'},
+	{"help", 0, 0, 'h'},
+	{0,0,0,0}
+};
+
 // Screen properties
-const uint16_t SCREEN_WIDTH = 1000;
-const uint16_t SCREEN_HEIGHT = 1000;
+uint16_t screen_width = 1000;
+uint16_t screen_height = 1000;
 const float SCREEN_MARGINS = 0.05;
 
 // Parameters of the Chaos Game
@@ -36,12 +49,12 @@ const double PRIME_2 = 24481;
 uint64_t stepping = 2500;
 uint16_t frame_delay_ms = 50;
 
-// flag_continue dictates the continuation of computation
+// flag_continue dictates the continuation of the game
 bool flag_continue = true;
 
 /**
 * Log an SDL error with some error message
-* @param msg The error message to write, format will be msg error: SDL_GetError()
+* @param msg: The error message to write, format will be msg error: SDL_GetError()
 */
 void log_SDL_error(const std::string &msg){
 	std::cerr << msg << " error: " << SDL_GetError() << std::endl;
@@ -56,27 +69,27 @@ void signal_interrupt(int _)
 	flag_continue = false;
 }
 
+double hash_point(int x, int y){
+	return x/PRIME_1 + y/PRIME_2;
+}
+
 int main(int argc, char *argv[]) {
 	signal(SIGINT, signal_interrupt);
 	srand(time(0));
 
 	// Process arguments
 	int opt, prev_ind;
-	while(prev_ind = optind, (opt = getopt(argc, argv, ":hs:d:v:f:")) != EOF)
+	while(prev_ind = optind, (opt = getopt_long(argc, argv, "hs:d:v:f:", long_opts, &optind)) != EOF)
 	{
-		if (optind == prev_ind + 2 && *optarg == '-') {
-			opt = ':';
-			--optind;
-		}
-		switch (opt)
-		{
+		switch(opt){
 			case 'h':
 				std::cout << "Options:" << std::endl;
-				std::cout << " -v N    number of vertices in the polygon (default: " << num_vertices << ")" << std::endl;
-				std::cout << " -f N    fraction of distance between the current point and chosen vertex to place a new point (default: " << factor << ")" << std::endl;
-				std::cout << " -s N    number of points to generate before refreshing the window (default: " << stepping << ")" << std::endl;
-				std::cout << " -d N    delay in ms after the window is refreshed (default: " << frame_delay_ms << ")" << std::endl;
-				std::cout << " -h      display this help page and exit" << std::endl;
+				std::cout << " -v N, --vertices N          number of vertices in the polygon (default: " << num_vertices << ")" << std::endl;
+				std::cout << " -f N, --fraction N          fraction of distance between the current point and chosen vertex to place a new point (default: " << factor << ")" << std::endl;
+				std::cout << " -s N, --stepping N          number of points to generate before refreshing the window (default: " << stepping << ")" << std::endl;
+				std::cout << " -d N, --frame-delay N       delay in ms after the window is refreshed (default: " << frame_delay_ms << ")" << std::endl;
+				std::cout << " --dimensions XxY            screen dimensions (default: " << screen_width << "x" << screen_height << ")" << std::endl;
+				std::cout << " -h, --help                  display this help page and exit" << std::endl;
 				std::cout << std::endl << std::endl;
 				return 0;
 
@@ -111,7 +124,7 @@ int main(int argc, char *argv[]) {
 				break;
 
 			case 'f':
-				if (std::atof(optarg) > 0.0 && std::atof(optarg) < 1.0){
+				if (std::atof(optarg) > 0.0 & std::atof(optarg) < 1.0){
 					factor = std::atof(optarg);
 					std::cout << "Factor set to " << factor << std::endl;
 				}
@@ -120,9 +133,24 @@ int main(int argc, char *argv[]) {
 				}
 				break;
 
-			case ':':
-				std::cerr << "Missing option. Exiting." << std::endl;
-				return 1;
+			case 'z':
+				// case for "dimensions" option, gathers dimensions from optarg in form "XxY".
+				std::stringstream ss(optarg);
+				std::string item;
+				std::vector<int> split_strings;
+				int num_items = 0;
+				while (std::getline(ss, item, 'x')){
+					split_strings.push_back(uint16_t (std::atoi(item.c_str())));
+					num_items++;
+				}
+				if (num_items != 2 || split_strings[0] <= 0 || split_strings[1] <= 0){
+					std::cout << "Invalid screen dimensions. Defaulting to " << screen_width << "x" << screen_height << std::endl;
+				}
+				else{
+					screen_width = split_strings[0];
+					screen_height = split_strings[1];
+				}
+				break;
 		}
 	}
 
@@ -133,7 +161,7 @@ int main(int argc, char *argv[]) {
 	SDL_Rect vertice_rects[num_vertices];
 
 	// Initialize SDL
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		log_SDL_error("SDL_Init");
 		return 1;
 	}
@@ -141,7 +169,7 @@ int main(int argc, char *argv[]) {
 	// Create window
 	SDL_Window *window = SDL_CreateWindow("Chaos Game",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		SCREEN_WIDTH, SCREEN_HEIGHT,
+		screen_width, screen_height,
 		0);
 	if (window == nullptr){
 		log_SDL_error("CreateWindow");
@@ -163,19 +191,19 @@ int main(int argc, char *argv[]) {
 	float theta;
 	for (int i = 0; i < num_vertices; i++){
 		theta = 270 + float (i * 360) / float (num_vertices);
-		x_point = SCREEN_WIDTH/2 + cos(theta*M_PI/180) * (SCREEN_WIDTH * (1.0 - SCREEN_MARGINS) / 2);
-		y_point = SCREEN_HEIGHT/2 + sin(theta*M_PI/180) * (SCREEN_HEIGHT * (1.0 - SCREEN_MARGINS) / 2);
+		x_point = screen_width/2 + cos(theta*M_PI/180) * (screen_width * (1.0 - SCREEN_MARGINS) / 2);
+		y_point = screen_height/2 + sin(theta*M_PI/180) * (screen_height * (1.0 - SCREEN_MARGINS) / 2);
 		vertices[i] = {uint16_t (x_point), uint16_t (y_point)};
 		vertice_rects[i] = SDL_Rect {x_point, y_point, RECTS_WIDTH, RECTS_HEIGHT};
 	}
 
 	// Reserve some space in the hashmap, which will likely be expanded
-	rects_hashmap.reserve(0.05 * SCREEN_WIDTH * SCREEN_HEIGHT);
+	rects_hashmap.reserve(0.05 * screen_width * screen_height);
 
 	// Create the first point
 	uint16_t last_x_point, last_y_point;
-	last_x_point = rand() % SCREEN_WIDTH;
-	last_y_point = rand() % SCREEN_HEIGHT;
+	last_x_point = rand() % screen_width;
+	last_y_point = rand() % screen_height;
 	rects.push_back(SDL_Rect {last_x_point, last_y_point, RECTS_WIDTH, RECTS_HEIGHT});
 	
 	// Keep generating points until flag_continue is set to false
@@ -191,8 +219,9 @@ int main(int argc, char *argv[]) {
 		
 		// Hash the new point's position and check if it has already been created.
 		// If the point does not exist, create it.
-		hash = x_point/PRIME_1 + y_point/PRIME_2;
-		if (rects_hashmap.find(hash) == rects_hashmap.end()){
+		// hash = x_point/PRIME_1 + y_point/PRIME_2;
+		hash = hash_point(x_point, y_point);
+		if (rects_hashmap[hash] == false){
 			rects_hashmap[hash] = true;
 			num_rects++;
 			rects.push_back(SDL_Rect {x_point, y_point, RECTS_WIDTH, RECTS_HEIGHT});
